3f1d59d60cdc31f48456a8c305a4d12073a9a721
giovaner10 <giovanerr10@gmail.com>
2022-05-03 12:53:28 -0300

    adocao do componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
 {
-  path: "login",
-  component: NavComponent
+  path: "",
+  component: NavComponent,
+  children: [
+    {
+      path: "home",
+      component: HomeComponent
+    }
+  ]
 }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
     
   ],
   imports: [
