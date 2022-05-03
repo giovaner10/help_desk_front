b8cc83bac9061366b4c4813ece3810698b4f3089
giovaner10 <giovanerr10@gmail.com>
2022-05-03 20:03:12 -0300

    list tecnicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
 {
@@ -11,6 +12,10 @@ const routes: Routes = [
     {
       path: "home",
       component: HomeComponent
+    },
+    {
+      path: "tecnicos",
+      component: TecnicoListComponent
     }
   ]
 }
