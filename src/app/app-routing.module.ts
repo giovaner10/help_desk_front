f353ad6a1bfb5372a0fc55bd3d42dc5f8dcba728
giovaner10 <giovanerr10@gmail.com>
2022-05-05 15:15:41 -0300

    rota para chamados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 const routes: Routes = [
 {
@@ -62,6 +63,12 @@ const routes: Routes = [
     {
       path: 'clientes/delete/:id',
       component: ClienteDeleteComponent
+    },
+
+
+    {
+      path: 'chamados',
+      component: ChamadoListComponent
     },
   ]
 
