269bdd8d7d916293981faa1682c31eec2ac89ed9
giovaner10 <giovanerr10@gmail.com>
2022-05-05 21:08:58 -0300

    ajuste na mensagem de erro

--- src/app/components/cliente/cliente-delete/cliente-delete.component.ts
+++ src/app/components/cliente/cliente-delete/cliente-delete.component.ts
@@ -45,14 +45,20 @@ export class ClienteDeleteComponent implements OnInit {
     this.service.delete(this.cliente.id).subscribe(() => {
       this.toast.success('Cliente deletado com sucesso', 'Delete');
       this.router.navigate(['clientes'])
-    }, ex => {
+    }, ex =>  {
       if(ex.error.errors) {
         ex.error.errors.forEach((element: { message: string | undefined; }) => {
           this.toast.error(element.message);
         });
       } else {
+        if(Number.parseInt(ex.error.status) == 403){
+          this.toast.error("Acesso a essa operação negado, sem permissão.");
+
+        }else{
         this.toast.error(ex.error.message);
+        }
       }
+
     })
   }
 
